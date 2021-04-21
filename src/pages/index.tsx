a2a8b9b0ffaeac74202b06e202679c7ef07fcc59
Guilherme GDP (DevGDP) <33395796+devgdp@users.noreply.github.com>
2021-04-20 22:05:33 -0300

    SSG - Consumindo API

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+// Formas de consumir API
+// Tradicional SPA - Implica no Crowler de busca para indexação do site
+// useEffect(() => {
+//   fetch('http://localhost:3333/episodes')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+// })
+
+// SSR 
+// export async function getServerSideProps(){
+//     const response = await fetch('http://localhost:3333/episodes')
+//     const data = await response.json()
+
+//     return { 
+//       props: {
+//         episodes: data,
+        
+//       }
+//     }
+// }
+    
+
+// SSG
+// Método Escolhido abaixo, criando uma parte estática da pagina usado quando a pagina nao muda constantemente, atualizando em um intervalo especifico
+
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 *8
+  }
+}
