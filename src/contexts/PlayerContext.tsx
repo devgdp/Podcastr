fb4e643f5d05cdfea967fdfe0c589cf3410a64bf
Guilherme GDP (DevGDP) <33395796+devgdp@users.noreply.github.com>
2021-04-23 21:55:26 -0300

    Funções PlayNetx - PlayPrevious

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,89 @@
+import { createContext, ReactNode, useState } from 'react';
+
+type Episode = {
+    title: string;
+    members: string;
+    thumbnail: string;
+    duration: number;
+    url: string;
+}
+
+type PlayerContextData =  {
+    episodeList: Episode[];
+    currentEpisodeIndex: number;
+    isPlaying: boolean;
+    play: (episode: Episode) => void;
+    playList: (list: Episode[], index: number) => void;
+    togglePlay: () => void;
+    playNext: () => void;
+    playPrevious: () => void;
+    setPlayingState: (state: boolean) => void;
+    hasNext: boolean;
+    hasPrevious: boolean;
+}
+
+type PlayerContextProviderProps = {
+    children: ReactNode;
+}
+export const PlayerContext = createContext({} as PlayerContextData);
+
+export function PlayerContextProvider({children} : PlayerContextProviderProps){
+    
+    const [episodeList, setEpisodeList] = useState([]);
+    const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    function play(episode: Episode){
+        setEpisodeList([episode]);
+        setCurrentEpisodeIndex(0);
+        setIsPlaying(true)
+    }
+
+    function playList(list: Episode[], index: number){
+        setEpisodeList(list);
+        setCurrentEpisodeIndex(index);
+        setIsPlaying(true)
+    }
+
+    const hasPrevious = currentEpisodeIndex > 0;
+    const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
+
+    function playNext(){
+        if(hasNext){
+            setCurrentEpisodeIndex(currentEpisodeIndex + 1)
+        }
+
+    }
+
+    function playPrevious(){
+        if(hasPrevious){
+            setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+        }
+    }
+
+    function togglePlay(){
+        setIsPlaying(!isPlaying);
+    }
+
+    function setPlayingState(state: boolean){
+        setIsPlaying(state);
+    }
+
+    return(
+        <PlayerContext.Provider value={{
+            episodeList, 
+            currentEpisodeIndex,
+            isPlaying, 
+            play,
+            playList,
+            playNext,
+            playPrevious,
+            togglePlay,
+            setPlayingState,
+            hasNext,
+            hasPrevious
+        }}>
+            {children}
+        </PlayerContext.Provider>      
+    )
+}
